104459d11512bb9c0109eb9e05119b3aa94c2f57
zengingrid@gmail.com <zengingrid@gmail.com>
2022-10-11 12:24:53 -0700

    data entry

---
+++ enterdata/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path');
+
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname,"public/views/login.html"))
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname,"public/views/register.html"))
+})
+
+app.get("/grades", (req, res) => {
+    res.sendFile(path.join(__dirname,"public/views/base.html"))
+})
+
+app.listen(8080, () => console.log("listening on port 8080"))
+
+module.exports = app;
