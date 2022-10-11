8c67b65b8e64fcb76fca374064e0f7ff70273758
BennyJiangzy <Bennyjiangzy@gmail.com>
2022-10-11 13:03:23 -0700

    changes

--- Authentication/app.py
+++ Authentication/app.py
@@ -3,7 +3,7 @@ import connexion
 import json
 from connexion import NoContent
 import yaml
-
+from flask_cors import CORS
 
 with open('app_conf.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
@@ -18,6 +18,7 @@ def username_password_info(body):
     return "Username or password doesn't match", 404
 
 app = connexion.FlaskApp(__name__, specification_dir='')
+CORS(app.app, resources={r"*": {"origins": "*", "headers": "*"}})
 app.add_api("openapi.yml",strict_validation=True,validate_responses=True)
 
 if __name__ == "__main__":
