5698edd1b917ad40af14eca30e81f3b75277dda4
zengingrid@gmail.com <zengingrid@gmail.com>
2022-10-11 22:49:06 -0700

    showdata

---
+++ showdata/app.js
@@ -0,0 +1,31 @@
+var express = require("express"),
+app = express(),
+bodyparser = require("body-parser"),
+mongoose = require("mongoose");
+var MongoClient = require('mongodb').MongoClient;
+
+const uri = "mongodb://mongodb:27017/grade";
+const client = new MongoClient(uri);
+const database = client.db("grade")
+const grades = database.collection("grade")
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+    res.render("index",{ allgrades: null })
+})
+
+app.get("/getgrades", function (req, res) {   
+    allgrades = grades.find()
+    console.log(allgrades)
+
+    if ((allgrades.count()) === 0) {
+        console.log("No documents found!");
+      } else {
+        res.render("index", { allgrades: allgrades })
+      }        
+    }
+)
+
+app.listen(3000, () => console.log("listening on http://localhost:3000"))
--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@ docker build -t assign/mysql Mysql/
 docker build -t assign/auth Authentication/
 docker build -t assign/analyze Analyze/
 docker build -t assign/enterdata enterdata/
+docker build -t assign/showdata showdata/
 docker network create -d bridge assignet
 docker-compose up
