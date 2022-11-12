50197777c5ba9c82f39368e07e325bbfcaf46f8e
BennyJiangzy <Bennyjiangzy@gmail.com>
2022-11-12 15:58:41 -0800

    pull add

---
+++ pull.sh
@@ -0,0 +1,7 @@
+docker pull bennyjiang/mysqlsv 
+docker pull bennyjiang/auth 
+docker pull bennyjiang/analyze 
+docker pull bennyjiang/enterdata 
+docker pull bennyjiang/showdata 
+docker pull bennyjiang/mysql
+docker pull bennyjiang/mongodb
