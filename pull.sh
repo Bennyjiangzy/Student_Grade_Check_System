e9c66d522f9df742fb8f2319a22d1ed9080c797d
BennyJiangzy <Bennyjiangzy@gmail.com>
2022-11-12 16:01:31 -0800

    pull add 2

--- pull.sh
+++ pull.sh
@@ -5,3 +5,12 @@ docker pull bennyjiang/enterdata
 docker pull bennyjiang/showdata 
 docker pull bennyjiang/mysql
 docker pull bennyjiang/mongodb
+
+
+docker tag bennyjiang/mysqlsv us.gcr.io/app-368216/cluster-1
+docker tag bennyjiang/auth us.gcr.io/app-368216/cluster-1
+docker tag bennyjiang/analyze us.gcr.io/app-368216/cluster-1
+docker tag bennyjiang/enterdata us.gcr.io/app-368216/cluster-1
+docker tag bennyjiang/showdata us.gcr.io/app-368216/cluster-1
+docker tag bennyjiang/mysql us.gcr.io/app-368216/cluster-1
+docker tag bennyjiang/mongodb us.gcr.io/app-368216/cluster-1
