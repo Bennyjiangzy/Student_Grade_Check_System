a83b5cd125f876aa6451634650273d7bec02227e
BennyJiangzy <Bennyjiangzy@gmail.com>
2022-11-12 16:06:38 -0800

    pull add 3

--- pull.sh
+++ pull.sh
@@ -7,10 +7,18 @@ docker pull bennyjiang/mysql
 docker pull bennyjiang/mongodb
 
 
-docker tag bennyjiang/mysqlsv us.gcr.io/app-368216/cluster-1
-docker tag bennyjiang/auth us.gcr.io/app-368216/cluster-1
-docker tag bennyjiang/analyze us.gcr.io/app-368216/cluster-1
-docker tag bennyjiang/enterdata us.gcr.io/app-368216/cluster-1
-docker tag bennyjiang/showdata us.gcr.io/app-368216/cluster-1
-docker tag bennyjiang/mysql us.gcr.io/app-368216/cluster-1
-docker tag bennyjiang/mongodb us.gcr.io/app-368216/cluster-1
+docker tag bennyjiang/mysqlsv us.gcr.io/app-368216/cluster-1/mysqlsv
+docker tag bennyjiang/auth us.gcr.io/app-368216/cluster-1/auth
+docker tag bennyjiang/analyze us.gcr.io/app-368216/cluster-1/analyze
+docker tag bennyjiang/enterdata us.gcr.io/app-368216/cluster-1/enterdata
+docker tag bennyjiang/showdata us.gcr.io/app-368216/cluster-1/showdata
+docker tag bennyjiang/mysql us.gcr.io/app-368216/cluster-1/mysql
+docker tag bennyjiang/mongodb us.gcr.io/app-368216/cluster-1/mongodb
+
+docker push us.gcr.io/app-368216/cluster-1/mysqlsv
+docker push us.gcr.io/app-368216/cluster-1/auth
+docker push us.gcr.io/app-368216/cluster-1/analyze
+docker push us.gcr.io/app-368216/cluster-1/enterdata
+docker push us.gcr.io/app-368216/cluster-1/showdata
+docker push us.gcr.io/app-368216/cluster-1/mysql
+docker push us.gcr.io/app-368216/cluster-1/mongodb
