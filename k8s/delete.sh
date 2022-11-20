cd023bda1622e710bdadb2db880ce5f6171f8833
BennyJiangzy <Bennyjiangzy@gmail.com>
2022-11-19 17:43:06 -0800

    test13

--- k8s/delete.sh
+++ k8s/delete.sh
@@ -8,3 +8,4 @@ kubectl delete pvc mongo-pvc
 kubectl delete pvc mysql-pvc
 kubectk delete pv mongo-pv
 kubectk delete pv mysql-pv
+kubectl delete horizontalpodautoscaler.autoscaling/auth-sv-backend-dep
