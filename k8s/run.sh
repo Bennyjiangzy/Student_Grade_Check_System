836a5416a97b873ee76590c9468c8f3cbe5fa2d0
BennyJiangzy <Bennyjiangzy@gmail.com>
2022-11-25 18:10:48 -0800

    test6

--- k8s/run.sh
+++ k8s/run.sh
@@ -1,7 +1,7 @@
 kubectl apply -f lb-ft.yml
 kubectl apply -f pd-ft.yml
 kubectl apply -f autoscale/bk-lb.yml
-kubectl apply -f autoscale/database-pv-pvc.yml
+# kubectl apply -f autoscale/database-pv-pvc.yml
 kubectl apply -f autoscale/bk-dep-pod.yml
 kubectl apply -f autoscale/bk-dep-pod2.yml
 kubectl apply -f autoscale/bk-hpa.yml
